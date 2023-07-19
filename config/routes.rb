e54f502ae9cea010664e84b8a62b76b891d88f1b
blackredgate <138416861+blackredgate@users.noreply.github.com>
2023-07-19 05:49:45 +0000

    [Finish]4-9

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,3 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-4
